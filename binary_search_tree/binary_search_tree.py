e82b84d2f6cf5d91375d71586604ca26e05b8a1c
yannafaith <yannafaith97@gmail.com>
2019-05-09 07:45:20 -0700

    finishes binary search tree / tests passing

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,68 @@
+# left less, right greater
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        current = self
+        while current.value != value:
+            if value > current.value:
+                if current.right:
+                    current = current.right
+                else:
+                    current.right = BinarySearchTree(value)
+                    
+            elif value < current.value:
+                if current.left:
+                    current = current.left
+                else:
+                    current.left = BinarySearchTree(value)
 
-  def contains(self, target):
-    pass
-
-  def get_max(self):
-    pass
-
-  def for_each(self, cb):
-    pass
+    def contains(self, target):
+        current = self
+        while current.value:
+            if current.value == target:
+                return True
+                break
+            
+            if current.value < target:
+                if current.right:
+                    current = current.right
+                else:
+                    return False
+                
+            if current.value > target:
+                if current.left:
+                    current = current.left
+                else:
+                    return False
+                
+    def get_max(self):
+        current = self
+        while current.right:
+            current = current.right  
+        return current.value
+    
+    def for_each(self, cb, node="initial"):
+        if node == "initial":
+              node = self
+        if node == None:
+              return
+        cb(node.value)
+        self.for_each(cb, node.left)
+        self.for_each(cb, node.right)
+        
+    def for_each(self, cb, node="root"):
+        # treats each node like a tree
+        if node == "root":
+              node = self
+        # can't recurse anymore
+        if node == None:
+              return
+        cb(node.value)
+        self.for_each(cb, node.left)
+        self.for_each(cb, node.right)
+        
+        
